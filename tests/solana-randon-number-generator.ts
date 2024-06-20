732eaa11de2e38ca3a8c141278fd90bb867f9a07
tarun-oasis <tarunjaiswal@oasispromarkets.com>
2024-06-20 14:43:21 +0530

    Working n switchboard-on-demand

--- tests/solana-randon-number-generator.ts
+++ tests/solana-randon-number-generator.ts
@@ -1,6 +1,13 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaRandonNumberGenerator } from "../target/types/solana_randon_number_generator";
+import {
+  AnchorUtils,
+  Queue,
+  Randomness,
+  SB_ON_DEMAND_PID,
+} from "@switchboard-xyz/on-demand";
+import { PublicKey, Connection } from "@solana/web3.js";
 
 // Create test keypairs
 const payer = anchor.web3.Keypair.generate();
@@ -43,6 +50,40 @@ describe("Random Number", () => {
     await confirmTransaction(generate);
   };
 
+  const generateWithSwitchBoard = async () => {
+    const sbQueue = new PublicKey(
+      "FfD96yeXs4cxZshoPPSKhSPgVQxLAJUT3gefgh84m1Di"
+    );
+    const sbProgramId = SB_ON_DEMAND_PID;
+    const sbIdl = await anchor.Program.fetchIdl(sbProgramId, provider);
+    const sbProgram = new anchor.Program(sbIdl!, provider);
+
+    // setup
+    const path = "sb-randomness/target/deploy/sb_randomness-keypair.json";
+    const [_, myProgramKeypair] = await AnchorUtils.initWalletFromFile(path);
+
+    const rngKp = anchor.web3.Keypair.generate();
+    const [randomness, ix] = await Randomness.create(sbProgram, rngKp, sbQueue);
+
+    // const sbProgram = await SwitchboardProgram.load(
+    //   // "mainnet-beta",
+    //   new Connection("https://api.devnet.solana.com"),
+    // );
+
+    // const aggregatorAccount = new AggregatorAccount(sbProgram, payer.publicKey);
+
+    let generate = await program.methods
+      .generateWithSwitchboard()
+      .accounts({
+        random: pdaRandom,
+        // aggregator: aggregatorAccount.publicKey
+        randomnessAccountData: randomness.pubkey,
+      })
+      .rpc();
+
+    await confirmTransaction(generate);
+  };
+
   it("Initialize test accounts", async () => {
     // Airdrop sol to the test users
     let payerSol = await provider.connection.requestAirdrop(
@@ -76,12 +117,18 @@ describe("Random Number", () => {
   });
 
   it("Test Random Number Generator", async () => {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 1; i++) {
       // Generate random number and store
       await generateAndStore();
       let random = await program.account.random.fetch(pdaRandom);
       console.log(Number(random.number));
       await sleep(1000);
     }
+  });
+
+  it("Test Random Number Generator with switchboard", async () => {
+    await generateWithSwitchBoard();
+    let random = await program.account.random.fetch(pdaRandom);
+    console.log(Number(random.number));
   });
 });
